174ccfa089d5c6662896249369218f46c03d16c6
Jonathan Benn <bonjennprojects@gmail.com>
2024-12-04 11:05:52 -0800

    Chaned onCancel

--- src/components/posts/CreatePostForm.tsx
+++ src/components/posts/CreatePostForm.tsx
@@ -6,7 +6,7 @@ import Image from 'next/image';
 
 interface CreatePostFormProps {
   onSuccess?: () => void;
-  _onCancel?: () => void;
+  onCancel?: () => void;
 }
 
 export const CreatePostForm = ({ onSuccess, _onCancel }: CreatePostFormProps) => {
