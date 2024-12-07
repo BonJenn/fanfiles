cfff81ae48a710de4e62ab42c4c62c27265d33af
Jonathan Benn <bonjennprojects@gmail.com>
2024-12-06 17:52:51 -0800

    Fixed management for production

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { createContext, useContext, useEffect, useState } from 'react';
-import { User } from '@supabase/supabase-js';
+import { User, Session } from '@supabase/supabase-js';
 import { supabase } from '@/lib/supabase';
 import { useRouter } from 'next/navigation';
 
