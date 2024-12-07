129fbaed598f0cb528f996616c8e87635329bf87
Jonathan Benn <bonjennprojects@gmail.com>
2024-12-06 21:52:52 -0800

    Implemented search functionality and wrapped components

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,16 +1,12 @@
 'use client';
 
-import { Suspense } from 'react';
 import { SearchWrapper } from '@/components/common/SearchWrapper';
-import { Spinner } from '@/components/common/Spinner';
 import { SignupContent } from '@/components/auth/SignupContent';
 
 export default function SignupPage() {
   return (
-    <Suspense fallback={<Spinner />}>
-      <SearchWrapper>
-        <SignupContent />
-      </SearchWrapper>
-    </Suspense>
+    <SearchWrapper>
+      <SignupContent />
+    </SearchWrapper>
   );
 }
--- src/components/common/SearchWrapper.tsx
+++ src/components/common/SearchWrapper.tsx
@@ -20,5 +20,3 @@ export function SearchWrapper({ children }: SearchWrapperProps) {
     </Suspense>
   );
 }
-</```
-rewritten_file>
